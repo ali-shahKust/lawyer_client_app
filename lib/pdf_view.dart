ad54ce12492783e6f9992feba261ce444dc56505
ali-shahKust <syed.ali.shah3938@gmail.com>
2020-03-04 22:20:34 +0500

    PDf Editor

--- lib/pdf_view.dart
+++ lib/pdf_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 class Pdf_viewer extends StatefulWidget {
 
-  String myUrl;
+ final String myUrl;
 
 
   Pdf_viewer({this.myUrl});
@@ -17,34 +17,74 @@ var pages ;
 var _controller;
 
 class _Pdf_viewerState extends State<Pdf_viewer> {
+  int _totalPages = 0;
+  int _currentPage = 0;
+  bool pdfReady = false;
+  PDFViewController _pdfViewController;
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: PDFView(
-        filePath: widget.myUrl,
-        enableSwipe: true,
-        swipeHorizontal: true,
-        autoSpacing: false,
-        pageFling: false,
-        onRender: (_pages) {
-          setState(() {
-            pages = _pages;
-            isReady = true;
-          });
-        },
-        onError: (error) {
-          print(error.toString());
-        },
-        onPageError: (page, error) {
-          print('$page: ${error.toString()}');
-        },
-        onViewCreated: (PDFViewController pdfViewController) {
-          _controller.complete(pdfViewController);
-        },
-        onPageChanged: (int page, int total) {
-          print('page change: $page/$total');
-        },
+      appBar: AppBar(
+        title: Text("My Document"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          PDFView(
+            filePath: widget.myUrl,
+            autoSpacing: true,
+            enableSwipe: true,
+            pageSnap: true,
+            swipeHorizontal: true,
+            nightMode: false,
+            onError: (e) {
+              print(e);
+            },
+            onRender: (_pages) {
+              setState(() {
+                _totalPages = _pages;
+                pdfReady = true;
+              });
+            },
+            onViewCreated: (PDFViewController vc) {
+              _pdfViewController = vc;
+            },
+            onPageChanged: (int page, int total) {
+              setState(() {});
+            },
+            onPageError: (page, e) {},
+          ),
+          !pdfReady
+              ? Center(
+            child: CircularProgressIndicator(),
+          )
+              : Offstage()
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          _currentPage > 0
+              ? FloatingActionButton.extended(
+            backgroundColor: Colors.red,
+            label: Text("Go to ${_currentPage - 1}"),
+            onPressed: () {
+              _currentPage -= 1;
+              _pdfViewController.setPage(_currentPage);
+            },
+          )
+              : Offstage(),
+          _currentPage+1 < _totalPages
+              ? FloatingActionButton.extended(
+            backgroundColor: Colors.green,
+            label: Text("Go to ${_currentPage + 1}"),
+            onPressed: () {
+              _currentPage += 1;
+              _pdfViewController.setPage(_currentPage);
+            },
+          )
+              : Offstage(),
+        ],
       ),
     );
   }
